56b685bc5ada65a8426de44bad79d5ca8bb15bbf
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:19:00 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "virtual_machine_gallery_application_assignments" {
+  description = "All virtual_machine_gallery_application_assignment resources"
+  value       = azurerm_virtual_machine_gallery_application_assignment.virtual_machine_gallery_application_assignments
+}
+
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "virtual_machine_gallery_application_assignments" {
+  description = <<EOT
+Map of virtual_machine_gallery_application_assignments, attributes below
+Required:
+    - gallery_application_version_id
+    - virtual_machine_id
+Optional:
+    - configuration_blob_uri
+    - order
+    - tag
+EOT
+
+  type = map(object({
+    gallery_application_version_id = string
+    virtual_machine_id             = string
+    configuration_blob_uri         = optional(string)
+    order                          = optional(number, 0)
+    tag                            = optional(string)
+  }))
+}
+
