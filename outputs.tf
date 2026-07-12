7c96a1b6c5c2ca6e87f454c13b6e9dcb1e6d89b3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:23 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "virtual_machine_gallery_application_assignments" {
-  description = "All virtual_machine_gallery_application_assignment resources"
-  value       = azurerm_virtual_machine_gallery_application_assignment.virtual_machine_gallery_application_assignments
-}
 output "virtual_machine_gallery_application_assignments_configuration_blob_uri" {
-  description = "List of configuration_blob_uri values across all virtual_machine_gallery_application_assignments"
-  value       = [for k, v in azurerm_virtual_machine_gallery_application_assignment.virtual_machine_gallery_application_assignments : v.configuration_blob_uri]
+  description = "Map of configuration_blob_uri values across all virtual_machine_gallery_application_assignments, keyed the same as var.virtual_machine_gallery_application_assignments"
+  value       = { for k, v in azurerm_virtual_machine_gallery_application_assignment.virtual_machine_gallery_application_assignments : k => v.configuration_blob_uri }
 }
 output "virtual_machine_gallery_application_assignments_gallery_application_version_id" {
-  description = "List of gallery_application_version_id values across all virtual_machine_gallery_application_assignments"
-  value       = [for k, v in azurerm_virtual_machine_gallery_application_assignment.virtual_machine_gallery_application_assignments : v.gallery_application_version_id]
+  description = "Map of gallery_application_version_id values across all virtual_machine_gallery_application_assignments, keyed the same as var.virtual_machine_gallery_application_assignments"
+  value       = { for k, v in azurerm_virtual_machine_gallery_application_assignment.virtual_machine_gallery_application_assignments : k => v.gallery_application_version_id }
 }
 output "virtual_machine_gallery_application_assignments_order" {
-  description = "List of order values across all virtual_machine_gallery_application_assignments"
-  value       = [for k, v in azurerm_virtual_machine_gallery_application_assignment.virtual_machine_gallery_application_assignments : v.order]
+  description = "Map of order values across all virtual_machine_gallery_application_assignments, keyed the same as var.virtual_machine_gallery_application_assignments"
+  value       = { for k, v in azurerm_virtual_machine_gallery_application_assignment.virtual_machine_gallery_application_assignments : k => v.order }
 }
 output "virtual_machine_gallery_application_assignments_tag" {
-  description = "List of tag values across all virtual_machine_gallery_application_assignments"
-  value       = [for k, v in azurerm_virtual_machine_gallery_application_assignment.virtual_machine_gallery_application_assignments : v.tag]
+  description = "Map of tag values across all virtual_machine_gallery_application_assignments, keyed the same as var.virtual_machine_gallery_application_assignments"
+  value       = { for k, v in azurerm_virtual_machine_gallery_application_assignment.virtual_machine_gallery_application_assignments : k => v.tag }
 }
 output "virtual_machine_gallery_application_assignments_virtual_machine_id" {
-  description = "List of virtual_machine_id values across all virtual_machine_gallery_application_assignments"
-  value       = [for k, v in azurerm_virtual_machine_gallery_application_assignment.virtual_machine_gallery_application_assignments : v.virtual_machine_id]
+  description = "Map of virtual_machine_id values across all virtual_machine_gallery_application_assignments, keyed the same as var.virtual_machine_gallery_application_assignments"
+  value       = { for k, v in azurerm_virtual_machine_gallery_application_assignment.virtual_machine_gallery_application_assignments : k => v.virtual_machine_id }
 }
 
